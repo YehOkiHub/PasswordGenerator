15a3d72ba8b1fad97bbd6a5fbc23b09ab0467f2f
Jeff <jeff077872@hotmail.com>
2022-09-17 14:20:45 -0400

    uploading version 3

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numBers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var upperCase = []
 
+var passLength;
 for(var i = 0; i < lowerCase.length; i++)
    upperCase[i] = lowerCase[i].toUpperCase()
 
 function generatePassword(){
 
  var userInput = window.prompt("Whats the length of your password?")
- var passLength = parseInt(userInput)
+ passLength = parseInt(userInput)
 
  if(isNaN(passLength) || passLength < 8 || passLength > 128) {
    window.alert("You must input a number between 8 and 128")
@@ -33,41 +34,46 @@ function generatePassword(){
 
  if(conLower === true) {
   
-   passList.concat(lowerCase);
+   passList = passList.concat(lowerCase);
 
  }
  if(conUpper === true) {
   
-  passList.concat(upperCase);
+  passList = passList.concat(upperCase);
 
 }
  if(conSymbol === true) {
   
-  passList.concat(symBols);
+  passList = passList.concat(symBols);
 
 }
  if(conNum === true) {
   
-  passList.concat(numBers);
+  passList = passList.concat(numBers);
 
 }
 
  if(conLower === false && conUpper === false && conSymbol === false && conNum === false){
 
   window.alert("Please choose at least one character type!");
-}
-
-console.log(passList);
-
- 
+  return;
 
 }
 
+var password = ""
+// var passwordArray = [];
+for(var i = 0; i < passLength; i++){
+password += (passList[Math.floor(Math.random() * passList.length)]);
+// randomPass = passList[Math.floor(Math.random()*passList.length)];
+
+console.log(password);
+}
+return password
+}
 
 
 
 
- 
 
 
 // Write password to the #password input
@@ -86,81 +92,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-
-
-//   function generatePassword() {
-
-//     var userInput = window.prompt("Choose your password Length")
-//     var passLength = parseInt(userInput)
-
-
-
-
-
-//    if (isNaN(passLength)) {
-//     window.alert("You must input a number")
-//     return
-//  }
-  
-//    if (passLength < 8 || passLength > 128){
-
-//   window.alert("Password length must be between 8-128 letters")
-//   return
-//  }
-
-  //  var conLower = window.confirm("Would you want Lowercase letters?")
-  //  var conUpper = window.confirm("Would you want Upper letters?")
-  //  var conSymbol = window.confirm("Would you want Symbols?")
-  //  var conNum = window.confirm("Would you want Numbers?")
-
-  //  var Symbols = ["!", "@", "#", "￥", "%", "^", "&", "*"]
-  //  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  //  var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-  //  var upperCase = []
-
-  //  for(var i = 0; i < lowerCase[i].length; i++)
-  //  upperCase[i] = lowerCase[i].toUpperCase()
-
-
-//   var finalPass = ""
-
-//    if(conLower === true){
-
-//   finalPass.push(lowerCase);
-//  }
-
-//  if(conUpper === true){
-//   finalPass.push(upperCase);
-
-//  if(conSymbol === true){
-
-//   finalPass.push(conSymbol);
-
-//  if(conNum === true){
-//   finalPass.push(Numbers);
-
-
-
-
-//  }
-
-// }
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
